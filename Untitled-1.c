e122a2460a08446ff69f339afa75fd26e191cefb
you1369-wu <wuyou0828@tongji.edu.cn>
2026-01-26 15:44:35 +0800

    c_exercise

---
+++ Untitled-1.c
@@ -0,0 +1,11 @@
+// 包含标准输入输出头文件，为printf函数提供声明
+#include <stdio.h>
+
+// 主函数，程序的入口点
+int main() {
+    // 输出 "hello world" 并换行
+    printf("hello world\n");
+    
+    // 返回0表示程序正常结束
+    return 0;
+}
